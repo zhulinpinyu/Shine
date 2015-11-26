b0e5d19c24bc5ff6b9a93a65c0e46300c99efd20
zhulinpinyu <mulixiang199016@gmail.com>
2015-11-26 16:20:14 +0800

    reimplementing pagination

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,10 +1,27 @@
 var app = angular.module('customers',[]);
 app.controller('CustomerSearchCtrl',function($scope,$http){
+  var page = 0;
   $scope.search = function(searchTerm){
     $scope.searchedFor = searchTerm;
-    $http.get('/customers.json',{"params":{"keywords": searchTerm}})
-    .then(function(response){
+    $http.get('/customers.json',
+      {
+        "params": {
+          "keywords": searchTerm,
+          "page": page
+        }
+      }
+    ).then(function(response){
       $scope.customers = response.data;
     });
+  };
+  $scope.previousPage = function(){
+    if(page > 0){
+      page = page - 1;
+      $scope.search($scope.keywords)
+    }
+  };
+  $scope.nextPage = function(){
+    page = page + 1;
+    $scope.search($scope.keywords)
   }
 });
