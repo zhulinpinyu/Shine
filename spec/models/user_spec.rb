7e2448bb71dd88a61b61a46d6c3176dd118187bb
zhulinpinyu <mulixiang199016@gmail.com>
2015-11-27 10:12:11 +0800

    refactor first test with matcher

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'support/violate_check_constraint_matcher'
 
 describe User do
   describe "email" do
@@ -8,7 +9,7 @@ describe User do
     it "absolutely prevents invalid email addresses" do
       expect{
         user.update_attribute(:email, "foo@bar.com")
-      }.to raise_error(ActiveRecord::StatementInvalid, /email_must_be_company_email/i)
+      }.to violate_check_constraint(:email_must_be_company_email)
     end
   end
 end
