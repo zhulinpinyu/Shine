5b95969b2b817d314d555e5375602ddb10a4ad81
zhulinpinyu <mulixiang199016@gmail.com>
2015-11-27 15:51:29 +0800

    test fetch search results

--- spec/javascripts/customers_app/controllers/customer_search_ctrl_spec.js
+++ spec/javascripts/customers_app/controllers/customer_search_ctrl_spec.js
@@ -13,4 +13,43 @@ describe("CustomerSearchCtrl",function(){
       expect(scope.customers).toEqualData([]);
     });
   });
+
+  describe("Fetching Search Results", function(){
+    var scope = null,
+        ctrl = null,
+        httpBackend = null,
+        serverResults = [
+          {
+            id: 123,
+            first_name: "Bob",
+            last_name: "Jones",
+            email: "bjones@foo.net",
+            username: "jonesy"
+          },
+          {
+            id: 456,
+            first_name: "Bob",
+            last_name: "Johnsons",
+            email: "johnboy@bar.info",
+            username: "bobbyj"
+          }
+        ];
+    beforeEach(module("customers"));
+    beforeEach(inject(function($controller, $rootScope, $httpBackend){
+      scope = $rootScope.$new();
+      httpBackend = $httpBackend;
+      ctrl = $controller("CustomerSearchCtrl",{
+        $scope: scope
+      })
+    }));
+    beforeEach(function(){
+      httpBackend.when('GET','/customers.json?keywords=bob&page=0').respond(serverResults);
+    });
+
+    it("populates the customer list with the results",function(){
+      scope.search("bob");
+      httpBackend.flush();
+      expect(scope.customers).toEqualData(serverResults);
+    });
+  });
 });
