ae6ee7b316ca23a8b393f319032cf20ecc16fd1c
zhulinpinyu <mulixiang199016@gmail.com>
2015-11-30 16:03:44 +0800

    update in rails Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'dashboard#index'
-  resources :customers, only: [:index, :show]
+  resources :customers, only: [:index, :show, :update]
   get "fake_billing", to: "fake_billing#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
