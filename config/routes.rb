cd6063b95e912084cd3a437539ab2c42bd8d43e1
zhulinpinyu <mulixiang199016@gmail.com>
2015-11-30 09:31:56 +0800

    mock bill info

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'dashboard#index'
   resources :customers, only: [:index, :show]
+  get "fake_billing", to: "fake_billing#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
